2b379c496e8d55a544fcfdf353c445c7bc861137
Arianna Formenti <ariannaformenti@lbl.gov>
2024-05-29 00:26:18 -0700

    clean up

--- examples/dummy/plot.py
+++ examples/dummy/plot.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np 
 import matplotlib.pyplot as plt 
 from matplotlib import use 
@@ -7,6 +8,11 @@ from openpmd_viewer import OpenPMDTimeSeries
 
 rcParams.update({'font.size': 12})
 
+plot_dir = 'plots'
+
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+   
 
 series = OpenPMDTimeSeries('./diags/diag1')
 iterations = series.iterations
@@ -32,7 +38,7 @@ for i, it in enumerate(iterations):
     ax.set_ylabel(info.axes[1]+' [m]')
     ax.set_title(f"t = {series.t[i]} s, step = {it}")    
     
-    fig.savefig(f'T_{i:04d}.png', dpi=300)
+    figname = os.path.join(plot_dir, f'T_{i:04d}.png')
+    fig.savefig(figname, dpi=300)
     plt.close()
-    
     
